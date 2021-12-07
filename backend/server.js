dbfc75439f497c2b48f3429831c2ba8afb4a1fac
Vidyansh31 <deva5084@gmail.com>
2021-12-07 18:03:46 +0530

    Connecting to database

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,17 @@ const app = require('./app');
 
 const dotenv = require('dotenv');
 
+//Importing database
+const connectDatabase = require('./config/database');
+
 // Creating port if it not listening on process.env.port than it will switch on 3000
 const port = process.env.PORT || 3000;
 
 // config
 dotenv.config({path:'backend/config/config.env'});
+
+//Connecting to Database
+connectDatabase();
 
 // Listening to the server on port
 app.listen(port,()=>{
